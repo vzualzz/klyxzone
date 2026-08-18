8c11027130f37bedc3d3c2165339a249d1458df8
vzualzz <mepeganasmoke7789@gmail.com>
2026-08-18 13:14:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const products = [
         price: 1349.91,
         desc: "Leve, preciso e com acabamento monocromático.",
         initial: "B"
+            image: "imagens1/beast1.webp"
       },
       {
         id: 2,
@@ -118,7 +119,11 @@ const products = [
       return `
         <article class="product">
           <div class="thumb">
-            <div class="fallback">${p.initial}</div>
+          ${p.image}" alt="$
+          {p.name}">`
+            :`<div class="fallback">$
+            {p.intitial}</div>`
+    }
           </div>
           <div class="content">
             <div class="tag">${p.category}</div>
