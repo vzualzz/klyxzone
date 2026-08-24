e6fe6b0ec6c29d3af8a6615b238b68ccf0d54826
vzualzz <mepeganasmoke7789@gmail.com>
2026-08-24 01:24:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const products = [
-  { id: 1, name: "Beast X Max", category: "mouses", price: 1349.91, desc: "Leve, preciso e com acabamento monocromático.", initial: "B", image: "imagens1/beast1.webp" },
+  { id: 1, name: "Beast X Max", category: "mouses", price: 1349.91, desc: "Leve, preciso e com acabamento monocromático.", initial: "B", image: "imagens1/beast1.webp", imagehover: "imagens1/beastblue.webp" },
   { id: 2, name: "ATK Ghost Extreme Carbon Fiber", category: "mouses", price: 989.91, desc: "Design enxuto para performance e controle.", initial: "A", image: "imagens1/atkghost.webp" },
   { id: 3, name: "Aspas RS6 Ultra HE", category: "teclados", price: 1889.91, desc: "Formato compacto com aparência premium.", initial: "A", image: "imagens1/aspasrs6.webp" },
   { id: 4, name: "ATK RS7 Air", category: "teclados", price: 701.91, desc: "Visual limpo com teclas de alta resposta.", initial: "R", image: "imagens1/rs7air.webp" },
@@ -64,12 +64,25 @@ function createProductCard(product) {
   thumb.className = "thumb";
 
   if (product.image) {
-    const image = document.createElement("img");
-    image.src = product.image;
-    image.alt = product.name;
-    image.loading = "lazy";
-    thumb.appendChild(image);
-  } else {
+
+  const image = document.createElement("img");
+  image.src = product.image;
+  image.alt = product.name;
+  image.loading = "lazy";
+  image.className = "main-image";
+
+  thumb.appendChild(image);
+
+  if (product.imageHover) {
+    const hoverImage = document.createElement("img");
+    hoverImage.src = product.imageHover;
+    hoverImage.alt = product.name;
+    hoverImage.loading = "lazy";
+    hoverImage.className = "hover-image";
+
+    thumb.appendChild(hoverImage);
+  } 
+  else {
     const fallback = document.createElement("div");
     fallback.className = "fallback";
     fallback.textContent = product.initial;
