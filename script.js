af76802633d06f8f41175c71d4a9d995266d8338
vzualzz <mepeganasmoke7789@gmail.com>
2026-09-05 19:26:40 -0300

    Reorder catalogOrder array elements

--- script.js
+++ script.js
@@ -209,8 +209,8 @@ function renderShelves() {
 }
 
 const catalogOrder = [
-  15, 16, 1, 2, 3, 4, 5, 6,
-  7, 8, 9, 10, 11, 12, 13, 14
+  13, 15, 16, 1, 2, 3, 4, 5, 6,
+  7, 8, 9, 10, 11, 12, 14
 ];
 
 function renderGrid() {
