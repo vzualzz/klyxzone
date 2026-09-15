e1f730e7c6c9192f4fb8f30f8904c21549fe7ac5
vzualzz <mepeganasmoke7789@gmail.com>
2026-09-15 14:16:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1542,30 +1542,3 @@ if (
     endDrag
   );
 }
-
-
-/* =======================================================
-   CARRINHO — BLOQUEIO DE TOUCH NO MOBILE
-   ======================================================= */
-
-document.addEventListener(
-  "touchmove",
-  (event) => {
-
-    if (window.innerWidth > 760) {
-      return;
-    }
-
-    if (!drawerEl.classList.contains("open")) {
-      return;
-    }
-
-    if (event.target.closest(".drawer-body")) {
-      return;
-    }
-
-    event.preventDefault();
-
-  },
-  { passive: false }
-);
