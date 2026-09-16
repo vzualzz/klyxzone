fbbba8ea361e3225428198f51e50a6ab0845d0c9
vzualzz <mepeganasmoke7789@gmail.com>
2026-09-16 18:06:04 -0300

    Update produto.js

--- produto.js
+++ produto.js
@@ -754,6 +754,22 @@ addButton.addEventListener(
 
     setTimeout(() => {
 
+  if (window.innerWidth <= 760) {
+
+    mobileScrollPosition =
+      window.scrollY;
+
+    document.body.style.position = "fixed";
+
+    document.body.style.top =
+      `-${mobileScrollPosition}px`;
+
+    document.body.style.left = "0";
+    document.body.style.right = "0";
+    document.body.style.width = "100%";
+
+  }
+
   drawer.classList.add("open");
 
   overlay.classList.add("open");
